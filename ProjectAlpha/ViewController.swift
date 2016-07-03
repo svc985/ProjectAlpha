146b1a3d907630f1f11b52f4aae9e278e9feab0b
Borislav Prikic <borislav.prikic@appculture.com>
2016-07-03 18:06:35 +0200

    Initial Commit

---
+++ ProjectAlpha/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProjectAlpha
+//
+//  Created by  Borislav Prikic on 7/3/16.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
