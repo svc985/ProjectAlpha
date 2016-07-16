19145e1f35bc1add967ea55b0ea2fa2df334f1fe
Borislav Prikic <borislav.prikic@appculture.com>
2016-07-16 16:22:45 +0200

    generating guessing label until it matches at least one DrawableUIView's values

--- ProjectAlpha/ViewController.swift
+++ ProjectAlpha/ViewController.swift
@@ -10,39 +10,50 @@ class ViewController: UIViewController {
     @IBOutlet weak var thirdView: DrawableUIView!
     @IBOutlet weak var fourthView: DrawableUIView!
     
+    @IBOutlet var drawableViews: [DrawableUIView]!
+    
+    var nextColorToGuess = Util.generateRandomColor()
+    var nextShapeToGuess = Util.generateRandomShape()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        //get initial color + object
-        let nextColorToGuess = Util.generateRandomColor()
-        let nextItemToShow =  Util.getColorNameFromRGBValues(nextColorToGuess) + " " + Util.generateRandomShape().rawValue
-        self.txtWhatObjectToShow.text = nextItemToShow
-        self.txtWhatObjectToShow.adjustsFontSizeToFitWidth = true
-        
         //generate initial set of drawings
         generateNextSetOfDrawings()
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        //get color + object to guess
+        generateWhatShouldBeGuessedLabel()
     }
     
     func generateNextSetOfDrawings() {
-        firstView.viewColor = Util.generateRandomColor()
-        firstView.viewShapeToDraw = Util.generateRandomShape()
-        
-        secondView.viewColor = Util.generateRandomColor()
-        secondView.viewShapeToDraw = Util.generateRandomShape()
-        
-        thirdView.viewColor = Util.generateRandomColor()
-        thirdView.viewShapeToDraw = Util.generateRandomShape()
-        
-        fourthView.viewColor = Util.generateRandomColor()
-        fourthView.viewShapeToDraw = Util.generateRandomShape()
-
+        for view in drawableViews {
+            view.viewColor = Util.generateRandomColor()
+            view.viewShapeToDraw = Util.generateRandomShape()
+        }
     }
-
+    
+    func generateWhatShouldBeGuessedLabel() {
+        while(checkIfNewGuessingLabelNeeded()) {
+            nextColorToGuess = Util.generateRandomColor()
+            nextShapeToGuess = Util.generateRandomShape()
+        }
+        
+        let nextItemToShow =  Util.getColorNameFromRGBValues(nextColorToGuess) + " " + nextShapeToGuess.rawValue
+        
+        self.txtWhatObjectToShow.textColor = Util.generateRandomColor()
+        self.txtWhatObjectToShow.backgroundColor = UIColor.lightGrayColor()
+        self.txtWhatObjectToShow.text = nextItemToShow
+        self.txtWhatObjectToShow.adjustsFontSizeToFitWidth = true
+    }
+    
+    func checkIfNewGuessingLabelNeeded() -> Bool {
+        for view in drawableViews {
+            if (Util.getColorNameFromRGBValues(view.viewColor) == Util.getColorNameFromRGBValues(nextColorToGuess) && view.viewShapeToDraw.rawValue == nextShapeToGuess.rawValue) {
+                return false
+            }
+        }
+        return true
+    }
 }
 
